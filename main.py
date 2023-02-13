48f780ae49278ffffb30ca50e590b95da3faba73
DDGUZMANO <douglasguzmanorence29@gmail.com>
2023-02-13 18:24:11 +0100

    En esta parte del ejercicio, se creó un objeto de clase vehículo y se guarda en un archivo .bin

---
+++ main.py
@@ -0,0 +1,19 @@
+import pickle
+
+class Vehiculo():
+    def __init__(self,color,modelo,marca):
+        self.color = color
+        self.modelo = modelo
+        self.marca = marca
+    def impr(self):
+        return f'Marca: {self.marca}\nModelo: {self.modelo}\nColor: {self.color}'
+        
+moto = Vehiculo('Negro','Hornet','Honda')
+print(moto.impr())
+
+f = open('fichero_vehiculos.bin','wb')
+pickle.dump(moto,f)
+f.close()
+
+    
+    
